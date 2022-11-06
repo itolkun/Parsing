4054d8411b8110f637b9c93fe2aff43acd8c298d
Aitolkun <totita@list.ru>
2022-11-06 13:52:27 +0300

    Initial Commit

---
+++ Parsing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Parsing
+//
+//  Created by Айтолкун Анарбекова on 6/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
